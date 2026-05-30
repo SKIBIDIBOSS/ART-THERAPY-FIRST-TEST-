1391ecf7c077f15ce1131cfd055db55386c4f6d7
SKIBIDIBOSS <aarav.nai66@gmail.com>
2026-05-30 08:11:08 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero";
+import AnimatedBackground from "@/components/AnimatedBackground";
+
+export default function Home() {
+ return (
+   <>
+     <AnimatedBackground />
+     <Hero />
+   </>
+ );
+}
